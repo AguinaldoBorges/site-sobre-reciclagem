6f2d70151dbd555fd5e96acdb99784c689ef5c57
AguinaldoBorges <tiaguinaldo2016@gmail.com>
2023-05-02 16:35:35 -0300

    ajustes efetuados nos cards

--- src/components/sections/AboutSection.vue
+++ src/components/sections/AboutSection.vue
@@ -3,19 +3,23 @@
         <!-- Content -->
         <div class="row  d-flex align-items-center pt-5 pb-5" id="about-row">
             <div class="col-12 d-flex text-center justify-content-center" style="padding-left:30px;">
-                <h2 class="" >
-                    <TitleTemplate :text="'Quem somos '" :color="'#CF1554'" :destakText="''" :destakColor="'#7BED9F'" :text2="''" class="section-one-text-container"/>
-                    <TextTemplate :text="'Estudantes de Pedagogia pela Faculdade Flamingo, tivemos como objetivo construir um puxa saco, uma palmeira e uma lixeira a partir de garrafas pet`s, a fim de mostrar a importância da reciclagem e da separação correta do lixo, conforme proposto pela disciplina de Metodologia de Ensino das Ciências Naturais. Nosso grupo é formado por: '" :color="'#252525'" class="pt-3 section-one-text-container"/>
+                <h2 class="">
+                    <TitleTemplate :text="'Quem somos '" :color="'#CF1554'" :destakText="''" :destakColor="'#7BED9F'"
+                        :text2="''" class="section-one-text-container" />
+                    <TextTemplate
+                        :text="'Estudantes de Pedagogia pela Faculdade Flamingo, tivemos como objetivo construir um puxa saco, uma palmeira e uma lixeira a partir de garrafas pet`s, a fim de mostrar a importância da reciclagem e da separação correta do lixo, conforme proposto pela disciplina de Metodologia de Ensino das Ciências Naturais. Nosso grupo é formado por: '"
+                        :color="'#252525'" class="pt-3 section-one-text-container" />
                 </h2>
             </div>
-            <div class="col-lg-3 col-md-6 col-sm-12 d-flex text-center justify-content-center" v-for="aluna in alunas" :key="aluna.index">
+            <div class="col-lg-3 col-md-6 col-sm-12 d-flex text-center justify-content-center" v-for="aluna in alunas"
+                :key="aluna.index">
                 <div class="card text-center mb-3" style="width: 99%;">
                     <div class="card-body">
-                        <img :src="aluna.foto" alt="foto da integrante do grupo" class="card-image"/>
-                      <h5 class="card-title">{{aluna.nome}}</h5>
-                      <p class="card-text">{{aluna.ra}}</p>
+                        <img :src="aluna.foto" alt="foto da integrante do grupo" class="card-image" />
+                        <h5 class="card-title">{{ aluna.nome }}</h5>
+                        <p class="card-text">{{ aluna.ra }}</p>
                     </div>
-                  </div>
+                </div>
             </div>
         </div>
 
@@ -32,26 +36,26 @@ export default {
     },
     data() {
         return {
-            alunas:[
+            alunas: [
                 {
-                    nome:'Fernanda Pereira Santos',
-                    ra:'RA: 22207875',
-                    foto:require('@/assets/3.png')
+                    nome: 'Fernanda Pereira Santos',
+                    ra: 'RA: 22207875',
+                    foto: require('@/assets/3.png')
                 },
                 {
-                    nome:'Ingrid de Carvalho de Souza',
-                    ra:'RA: 18203639',
-                    foto:require('@/assets/2.png')
+                    nome: 'Ingrid de Carvalho de Souza',
+                    ra: 'RA: 18203639',
+                    foto: require('@/assets/2.png')
                 },
                 {
-                    nome:'Letícia Ap. G. de A. S. R. Borges',
-                    ra:'RA: 22107690',
-                    foto:require('@/assets/4.png')
+                    nome: 'Letícia Ap. G. de A. S. R. Borges',
+                    ra: 'RA: 22107690',
+                    foto: require('@/assets/4.png')
                 },
                 {
-                    nome:'Sabrina Ferreira Maximiano',
-                    ra:'RA: 22207865',
-                    foto:require('@/assets/1.png')
+                    nome: 'Sabrina Ferreira Maximiano',
+                    ra: 'RA: 22207865',
+                    foto: require('@/assets/1.png')
                 }
             ]
         }
@@ -73,20 +77,31 @@ export default {
     flex-wrap: wrap;
     justify-content: center;
 }
-#about-row{
+
+#about-row {
     min-height: 100vh;
 
 }
 
-.about-text-container{
-    padding:0 70px;
-    padding-left:15px;
+.about-text-container {
+    padding: 0 70px;
+    padding-left: 15px;
 }
-.card{
-    -webkit-box-shadow: 0px 10px 13px -7px #000000, -50px -50px 0px -29px rgba(0,0,0,0);
-    box-shadow: 0px 10px 13px -7px #000000, -50px -50px 0px -29px rgba(0,0,0,0);
+
+.card {
+    -webkit-box-shadow: 0px 10px 13px -7px #000000, -50px -50px 0px -29px rgba(0, 0, 0, 0);
+    box-shadow: 0px 10px 13px -7px #000000, -50px -50px 0px -29px rgba(0, 0, 0, 0);
 }
-.card-image{
+
+.card :hover {
+    background-color: #7BED9F;
+    color: #252525;
+}
+
+.card-image {
     width: 250px;
 }
-</style>
+
+.card-title {
+    color: #cf2954;
+}</style>
